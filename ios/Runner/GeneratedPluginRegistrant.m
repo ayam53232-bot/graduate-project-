2d504141bd6f8e3376d5b1f7eff920e87f3ddfef
aya hebish <ayam53232@gmail.com>
2026-08-28 16:09:05 +0300

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<gal/GalPlugin.h>)
+#import <gal/GalPlugin.h>
+#else
+@import gal;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [GalPlugin registerWithRegistrar:[registry registrarForPlugin:@"GalPlugin"]];
+}
+
+@end
